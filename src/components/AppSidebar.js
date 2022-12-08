c02abda03e28e8715078e2aad7ac238109eaf4e0
nguyenhuan2102 <nguyenvanhuan2122001@gmail.com>
2022-12-08 13:37:16 +0700

    exam

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -32,16 +32,28 @@ const AppSidebar = () => {
         dispatch({ type: "set", sidebarShow: visible });
       }}
     >
-      <br />
-      <br />
-      <div className="text-center">
-        <b>HỆ THỐNG</b>
-      </div>
-      <div className="text-center">
-        <b>QUẢN LÍ TRƯỜNG HỌC</b>
-      </div>
+      <div
+        style={{
+          textAlign: "center",
 
-      <br />
+          color: "#e0dfdc",
+
+          letterSpacing: ".1em",
+          textShadow:
+            "0 -1px 0 #fff, 0 1px 0 #2e2e2e, 0 2px 0 #2c2c2c, 0 3px 0 #2a2a2a, 0 4px 0 #282828, 0 5px 0 #262626, 0 6px 0 #242424, 0 7px 0 #222, 0 8px 0 #202020, 0 9px 0 #1e1e1e, 0 10px 0 #1c1c1c, 0 11px 0 #1a1a1a, 0 12px 0 #181818, 0 13px 0 #161616, 0 14px 0 #141414, 0 15px 0 #121212, 0 22px 30px rgba(0, 0, 0, 0.9)",
+        }}
+      >
+        <br />
+        <br />
+        <div className="text-center">
+          <b>HỆ THỐNG</b>
+        </div>
+        <div className="text-center">
+          <b>QUẢN LÍ TRƯỜNG HỌC</b>
+        </div>
+
+        <br />
+      </div>
       <div className="text-dark">----------------------------------------</div>
       <CSidebarNav>
         <SimpleBar>
